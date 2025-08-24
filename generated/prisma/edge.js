9eee4ac6d59a817a6bfe03fe60c932fe81b9f6c8
Trần Trân Lộc <tran26122003@gmail.com>
2025-08-24 14:54:48 +0700

    conect woochat v1

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -344,7 +344,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
